7aeac04ff1f617f106269ff7d58012135b05e2fd
admbequest <127445261+admbequest@users.noreply.github.com>
2023-06-01 15:41:14 -0300

    Update investorsapp.py

--- investorsapp.py
+++ investorsapp.py
@@ -1,38 +1,18 @@
-from flask import Flask, request
-from slackeventsapi import SlackEventAdapter
 from slack_sdk import WebClient
 import os
 
-# Configurar o adaptador de eventos
-slack_signing_secret = os.environ["SLACK_SIGNING_SECRET"]
-slack_events_adapter = SlackEventAdapter(slack_signing_secret, "/slack/events")
-
-# Configurar o cliente Slack
+# Configurar o token do bot do Slack
 slack_bot_token = os.environ["SLACK_BOT_TOKEN"]
 client = WebClient(token=slack_bot_token)
 
-# Criar o objeto Flask
-app = Flask(__name__)
+# ID do canal ou grupo onde a mensagem será enviada
+channel_id = "C05ATA6JHDW"
 
-# Rota para receber as solicitações do Slack
-@app.route("/slack/events", methods=["POST"])
-def slack_events():
-    # Verificar se a solicitação é válida
-    slack_events_adapter.handle(request)
+# Enviar a mensagem
+response = client.chat_postMessage(channel=channel_id, text="Oi")
 
-    return "", 200
-
-# Manipulador de eventos
-@slack_events_adapter.on("app_mention")
-def handle_app_mention(event_data):
-    event = event_data["event"]
-    channel_id = event["channel"]
-
-    # Adicionar mensagem de depuração
-    print("Evento de menção do aplicativo recebido!")
-
-    # Enviar a resposta
-    client.chat_postMessage(channel=channel_id, text="Olá!")
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000-4000)
+# Verificar se a mensagem foi enviada com sucesso
+if response["ok"]:
+    print("Mensagem enviada com sucesso!")
+else:
+    print("Falha ao enviar mensagem:", response["error"])
