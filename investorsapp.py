30373b33c0702d63ac91d0b29c30a7b4bcbef1ed
admbequest <127445261+admbequest@users.noreply.github.com>
2023-06-01 14:45:20 -0300

    Update investorsapp.py

--- investorsapp.py
+++ investorsapp.py
@@ -1,17 +1,9 @@
 
 
-#!pip install slack_sdk
-
-#!pip install slackeventsapi
-
-#!pip install pydo
-
-#RESPONDER MENÇÃO COM OLÁ
-
+from investorsapp import Flask
 from slackeventsapi import SlackEventAdapter
 from slack_sdk import WebClient
 import os
-from pydo import wsgi
 
 # Configurar o adaptador de eventos
 slack_signing_secret = os.environ["SLACK_SIGNING_SECRET"]
@@ -21,16 +13,25 @@ slack_events_adapter = SlackEventAdapter(slack_signing_secret, "/slack/events")
 slack_bot_token = os.environ["SLACK_BOT_TOKEN"]
 client = WebClient(token=slack_bot_token)
 
+# Criar o objeto Flask
+app = Flask("investorsapp")
+
+# Rota para receber as solicitações do Slack
+@app.route("/slack/events", methods=["POST"])
+def slack_events():
+    # Verificar se a solicitação é válida
+    slack_events_adapter.handle(request)
+
+    return "", 200
+
 # Manipulador de eventos
 @slack_events_adapter.on("app_mention")
 def handle_app_mention(event_data):
     event = event_data["event"]
     channel_id = event["channel"]
-    user_id = event["user"]
 
     # Enviar a resposta
     client.chat_postMessage(channel=channel_id, text="Olá!")
 
-# Iniciar o servidor de eventos usando a biblioteca pydo
 if __name__ == "__main__":
-    wsgi.server(slack_events_adapter, port=3000)
+    app.run(host="0.0.0.0", port=3000)
