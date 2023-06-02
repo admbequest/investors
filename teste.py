c914925625b973030043873547ac2da54c142077
admbequest <127445261+admbequest@users.noreply.github.com>
2023-06-02 11:09:09 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,14 +1,20 @@
 from slack_sdk import WebClient
+import os
 
-# Defina o token de autenticação do seu aplicativo do Slack
-slack_token = "xoxb-1666887890084-5336678265829-YaKNeHXpRxk2QXrPAvidxHp6"
+# Configurar o token do bot do Slack
+slack_bot_token = os.environ["SLACK_BOT_TOKEN"]
+client = WebClient(token=slack_bot_token)
 
-# Crie uma nova instância do cliente Slack
-client = WebClient(token=slack_token)
+# ID do canal ou grupo onde a mensagem será enviada
+channel_id = "C05ATA6JHDW"
 
-# Envie uma mensagem no canal desejado
-channel_id = "C05ATA6JHDW" 
-message = "Oi! Tudo bem?"
-response = client.chat_postMessage(channel=channel_id, text=message)
+# Enviar a mensagem
+response = client.chat_postMessage(channel=channel_id, text="Oi")
+
+# Verificar se a mensagem foi enviada com sucesso
+if response["ok"]:
+    print("Mensagem enviada com sucesso!")
+else:
+    print("Falha ao enviar mensagem:", response["error"])
 
 
