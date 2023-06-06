3933aaaf5bbea1706071632ac3010295e4cbf837
admbequest <127445261+admbequest@users.noreply.github.com>
2023-06-06 11:32:14 -0300

    Update investorsapp.py

--- investorsapp.py
+++ investorsapp.py
@@ -1,7 +1,7 @@
 import os
 from slack_bolt import App
 from slack_bolt.adapter.flask import SlackRequestHandler
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 # Criar o objeto de aplicativo Flask
 flask_app = Flask(__name__)
@@ -18,16 +18,13 @@ def slack_events():
     # Obter a solicitação do Slack
     slack_request = handler.handle(request)
 
-    # Verificar se a solicitação é uma verificação de evento
-    if slack_request.command == "/slack/events" and slack_request.body.get("type") == "url_verification":
-        return slack_request.body.get("challenge")
+    # Verificar se a solicitação contém um challenge
+    if slack_request.body.get("challenge"):
+        challenge = slack_request.body["challenge"]
+        return jsonify({"challenge": challenge})
 
-    # Processar os eventos do Slack
-    response = app.dispatch(slack_request)
-    if response.status_code < 400:
-        return ""
-    else:
-        return response.body
+    # Confirmar o recebimento do evento
+    return jsonify(), 200
 
 # Manipulador de eventos
 @app.event("app_mention")
